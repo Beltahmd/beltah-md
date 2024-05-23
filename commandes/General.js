a7b4ac108db51e2e4b1024506c2daedae6c6f564
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-05-23 00:00:24 +0000

    Update General.js

--- commandes/General.js
+++ commandes/General.js
@@ -55,12 +55,12 @@ zokou({ nomCom: "dev", categorie: "General", reaction: "💞" }, async (dest, zk
     const { ms, mybotpic } = commandeOptions;
 
     const devs = [
-      { nom: "Djalega++", numero: "22559763447" },
+      { nom: "Supremus Prod", numero: "22501373930" },
       { nom: "᚛M๏𝓷keℽ D Lบffy᚜", numero: "22891733300" },
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "👋 welcome to Zokou ! here is the dev :\n\n";
+    let message = "👋 welcome to SP-ZK-MD ! here is the dev :\n\n";
     for (const dev of devs) {
       message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
     }
@@ -95,7 +95,7 @@ zokou({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOpti
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
   repondre("look on pm sir ")
-  await zk.sendMessage(auteurMessage,{text : `https://chat.whatsapp.com/H6oeuhfSMtV1Orjmf2NVnl`},{quoted :ms})
+  await zk.sendMessage(auteurMessage,{text : `https://whatsapp.com/channel/0029VabQzL67dmehHEH2DR3h`},{quoted :ms})
 
 })
 
