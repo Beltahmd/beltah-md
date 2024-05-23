6c563e77269c305ce09a6d2134786d1edb7cb30e
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-05-23 01:29:36 +0000

    Update alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -1,94 +1,116 @@
 const { zokou } = require('../framework/zokou');
-const {addOrUpdateDataInAlive , getDataFromAlive} = require('../bdd/alive')
+const { addOrUpdateDataInAlive, getDataFromAlive, addOrUpdateDataInTeste, getDataFromTeste } = require('../bdd/alive');
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
 
 zokou(
-    {
-        nomCom : 'alive',
-        categorie : 'General'
-        
-    },async (dest,zk,commandeOptions) => {
+  {
+    nomCom: 'alive',
+    categorie: 'General'
+  }, async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre, superUser } = commandeOptions;
 
- const {ms , arg, repondre,superUser} = commandeOptions;
+    const data = await getDataFromAlive();
 
- const data = await getDataFromAlive();
-
- if (!arg || !arg[0] || arg.join('') === '') {
-
-    if(data) {
-       
-        const {message , lien} = data;
-
+    if (!arg || !arg[0] || arg.join('') === '') {
+      if (data) {
+        const { message, lien } = data;
 
         var mode = "public";
         if ((s.MODE).toLocaleLowerCase() != "yes") {
-            mode = "private";
+          mode = "private";
         }
-      
-    
-     
-    moment.tz.setDefault('Etc/GMT');
 
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
+        moment.tz.setDefault('Etc/GMT');
 
-    const alivemsg = `
+        const temps = moment().format('HH:mm:ss');
+        const date = moment().format('DD/MM/YYYY');
+
+        const alivemsg = `
 *Owner* : ${s.OWNER_NAME}
 *Mode* : ${mode}
 *Date* : ${date}
 *Hours(GMT)* : ${temps}
 
- ${message}
- 
- 
- *Zokou_MD version 2.0*`
+${message}
 
- if (lien.match(/\.(mp4|gif)$/i)) {
-    try {
-        zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Checking for .jpeg or .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(alivemsg);
-    
-}
+*Zokou_MD version 2.0*`;
 
+        if (lien.match(/\.(mp4|gif)$/i)) {
+          try {
+            zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
+          } catch (e) {
+            console.log("🥵🥵 Menu erreur " + e);
+            repondre("🥵🥵 Menu erreur " + e);
+          }
+        } else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+          try {
+            zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
+          } catch (e) {
+            console.log("🥵🥵 Menu erreur " + e);
+            repondre("🥵🥵 Menu erreur " + e);
+          }
+        } else {
+          repondre(alivemsg);
+        }
+      } else {
+        if (!superUser) {
+          repondre("there is no alive for this bot");
+          return;
+        }
+
+        await repondre("You have not yet saved your alive, to do this; enter after alive your message and your image or video link in this context: .alive message;lien");
+        repondre("don't do fake things :)");
+      }
     } else {
-        if(!superUser) { repondre("there is no alive for this bot") ; return};
+      if (!superUser) {
+        repondre("Only the owner can modify the alive");
+        return;
+      }
 
-      await   repondre("You have not yet saved your alive, to do this;  enter after alive your message and your image or video link in this context: .alive message;lien");
-         repondre("don't do fake thinks :)")
-     }
- } else {
+      const texte = arg.join(' ').split(';')[0];
+      const tlien = arg.join(' ').split(';')[1];
 
-    if(!superUser) { repondre ("Only the owner can  modify the alive") ; return};
+      await addOrUpdateDataInAlive(texte, tlien);
 
-  
-    const texte = arg.join(' ').split(';')[0];
-    const tlien = arg.join(' ').split(';')[1]; 
+      repondre('message alive refresh successfully');
+    }
+  }
+);
 
+zokou(
+  {
+    nomCom: 'teste',
+    categorie: 'General'
+  }, async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre, superUser } = commandeOptions;
 
-    
-await addOrUpdateDataInAlive(texte , tlien)
+    const data = await getDataFromTeste();
 
-repondre('message alive refresh successfully')
+    if (!arg || !arg[0] || arg.join('') === '') {
+      if (data) {
+        const { message, lien } = data;
 
-}
-    });
+        var mode = "public";
+        if ((s.MODE).toLocaleLowerCase() != "yes") {
+          mode = "private";
+        }
+
+        moment.tz.setDefault('Etc/GMT');
+
+        const temps = moment().format('HH:mm:ss');
+        const date = moment().format('DD/MM/YYYY');
+
+        const testeMsg = `
+*Owner* : ${s.OWNER_NAME}
+*Mode* : ${mode}
+*Date* : ${date}
+*Hours(GMT)* : ${temps}
+
+${message}
+
+*Zokou_MD version 2.0*`;
+
+        if (lien.match(/\.(mp4|gif)$/i)) {
+          try {
+            zk.sendMessage
