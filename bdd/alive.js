1bdf75e65c5c9f5a6e0bb064caa3958ddae0dd1d
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-05-23 01:32:16 +0000

    Update alive.js

--- bdd/alive.js
+++ bdd/alive.js
@@ -7,7 +7,7 @@ const { Pool } = require("pg");
 const s = require("../set");
 
 // Récupérez l'URL de la base de données de la variable s.DATABASE_URL
-var dbUrl=s.DATABASE_URL?s.DATABASE_URL:"postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9"
+var dbUrl = s.DATABASE_URL ? s.DATABASE_URL : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9";
 const proConfig = {
   connectionString: dbUrl,
   ssl: {
@@ -20,73 +20,129 @@ const pool = new Pool(proConfig);
 
 // Fonction pour créer la table "alive" avec une colonne "id"
 const creerTableAlive = async () => {
-    try {
-      await pool.query(`
-        CREATE TABLE IF NOT EXISTS alive (
-          id serial PRIMARY KEY,
-          message text,
-          lien text
-        );
-      `);
-      console.log("La table 'alive' a été créée avec succès.");
-    } catch (e) {
-      console.error("Une erreur est survenue lors de la création de la table 'alive':", e);
-    }
-  };
-  
-  // Appelez la méthode pour créer la table "alive"
-  creerTableAlive();
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS alive (
+        id serial PRIMARY KEY,
+        message text,
+        lien text
+      );
+    `);
+    console.log("La table 'alive' a été créée avec succès.");
+  } catch (e) {
+    console.error("Une erreur est survenue lors de la création de la table 'alive':", e);
+  }
+};
+
+// Fonction pour créer la table "teste" avec une colonne "id"
+const creerTableTeste = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS teste (
+        id serial PRIMARY KEY,
+        message text,
+        lien text
+      );
+    `);
+    console.log("La table 'teste' a été créée avec succès.");
+  } catch (e) {
+    console.error("Une erreur est survenue lors de la création de la table 'teste':", e);
+  }
+};
+
+// Appelez les méthodes pour créer les tables "alive" et "teste"
+creerTableAlive();
+creerTableTeste();
 
 // Fonction pour ajouter ou mettre à jour un enregistrement dans la table "alive"
 async function addOrUpdateDataInAlive(message, lien) {
-    const client = await pool.connect();
-    try {
-      // Insérez ou mettez à jour les données dans la table "alive"
-      const query = `
-        INSERT INTO alive (id, message, lien)
-        VALUES (1, $1, $2)
-        ON CONFLICT (id)
-        DO UPDATE SET message = excluded.message, lien = excluded.lien;
-      `;
-      const values = [message, lien];
-  
-      await client.query(query, values);
-      console.log("Données ajoutées ou mises à jour dans la table 'alive' avec succès.");
-    } catch (error) {
-      console.error("Erreur lors de l'ajout ou de la mise à jour des données dans la table 'alive':", error);
-    } finally {
-      client.release();
-    }
-  };
+  const client = await pool.connect();
+  try {
+    const query = `
+      INSERT INTO alive (id, message, lien)
+      VALUES (1, $1, $2)
+      ON CONFLICT (id)
+      DO UPDATE SET message = excluded.message, lien = excluded.lien;
+    `;
+    const values = [message, lien];
 
- 
-  async function getDataFromAlive() {
-    const client = await pool.connect();
-    try {
-      // Exécutez la requête SELECT pour récupérer les données
-      const query = "SELECT message, lien FROM alive WHERE id = 1";
-      const result = await client.query(query);
-  
-      if (result.rows.length > 0) {
-        const { message, lien } = result.rows[0];
-        return { message, lien };
-      } else {
-        console.log("Aucune donnée trouvée dans la table 'alive'.");
-        return null;
-      }
-    } catch (error) {
-      console.error("Erreur lors de la récupération des données depuis la table 'alive':", error);
+    await client.query(query, values);
+    console.log("Données ajoutées ou mises à jour dans la table 'alive' avec succès.");
+  } catch (error) {
+    console.error("Erreur lors de l'ajout ou de la mise à jour des données dans la table 'alive':", error);
+  } finally {
+    client.release();
+  }
+}
+
+// Fonction pour ajouter ou mettre à jour un enregistrement dans la table "teste"
+async function addOrUpdateDataInTeste(message, lien) {
+  const client = await pool.connect();
+  try {
+    const query = `
+      INSERT INTO teste (id, message, lien)
+      VALUES (1, $1, $2)
+      ON CONFLICT (id)
+      DO UPDATE SET message = excluded.message, lien = excluded.lien;
+    `;
+    const values = [message, lien];
+
+    await client.query(query, values);
+    console.log("Données ajoutées ou mises à jour dans la table 'teste' avec succès.");
+  } catch (error) {
+    console.error("Erreur lors de l'ajout ou de la mise à jour des données dans la table 'teste':", error);
+  } finally {
+    client.release();
+  }
+}
+
+// Fonction pour récupérer les données depuis la table "alive"
+async function getDataFromAlive() {
+  const client = await pool.connect();
+  try {
+    const query = "SELECT message, lien FROM alive WHERE id = 1";
+    const result = await client.query(query);
+
+    if (result.rows.length > 0) {
+      const { message, lien } = result.rows[0];
+      return { message, lien };
+    } else {
+      console.log("Aucune donnée trouvée dans la table 'alive'.");
       return null;
-    } finally {
-      client.release();
     }
-  };
-  
-  
-  
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données depuis la table 'alive':", error);
+    return null;
+  } finally {
+    client.release();
+  }
+}
 
-  module.exports = {
-    addOrUpdateDataInAlive,
-    getDataFromAlive,
-    
-  };
+// Fonction pour récupérer les données depuis la table "teste"
+async function getDataFromTeste() {
+  const client = await pool.connect();
+  try {
+    const query = "SELECT message, lien FROM teste WHERE id = 1";
+    const result = await client.query(query);
+
+    if (result.rows.length > 0) {
+      const { message, lien } = result.rows[0];
+      return { message, lien };
+    } else {
+      console.log("Aucune donnée trouvée dans la table 'teste'.");
+      return null;
+    }
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données depuis la table 'teste':", error);
+    return null;
+  } finally {
+    client.release();
+  }
+}
+
+module.exports = {
+  addOrUpdateDataInAlive,
+  getDataFromAlive,
+  addOrUpdateDataInTeste,
+  getDataFromTeste,
+};
