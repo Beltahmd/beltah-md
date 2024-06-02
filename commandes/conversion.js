57b7bfbb3d93debfe997812bd69492752f5113e3
Beltahmd <168731091+Beltahmd@users.noreply.github.com>
2024-06-02 07:29:51 +0300

    Update conversion.js

--- commandes/conversion.js
+++ commandes/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Zokou-Md" ,
+      pack:"Beltah-Md" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
@@ -94,7 +94,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Zokou-Md", // pack stick
+      pack:"Beltah-Md", // pack stick
       author:  nomAuteurMessage, // name of the author of the stick
       type:
         arg.includes("-r") || arg.includes("-c")
