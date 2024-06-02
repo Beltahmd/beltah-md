9afb1834106318a6db64edeb577c2e2e3f890024
Beltahmd <168731091+Beltahmd@users.noreply.github.com>
2024-06-02 07:41:05 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "❄️";
+        infoComs.reaction = "📲";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
