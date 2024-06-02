e7d41a17738411b0c713132a83529f01732467e8
Beltahmd <168731091+Beltahmd@users.noreply.github.com>
2024-06-02 06:53:53 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 zokou-md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Zokou-md', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 Beltah-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Beltah Tech Team', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
