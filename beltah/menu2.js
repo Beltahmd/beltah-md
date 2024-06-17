b669878744b3c5627353793b215f5eb7671ce4b9
Beltahmd <168731091+Beltahmd@users.noreply.github.com>
2024-06-18 00:02:30 +0300

    Create menu2.js

---
+++ beltah/menu2.js
@@ -0,0 +1,102 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+zokou({ nomCom: "menu2", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+ 𓄂𓄂𓄂《《 *𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 V3.0.0* 》》𓄂𓄂𓄂
+┴╭─────────────✣
+│☹︎│▸ *BOT OWNER* : ${s.OWNER_NAME}
+│☹︎│▸ *DATE TODAY* : ${date}
+│☹︎│▸ *BOT PREFIX* : ${s.PREFIXE}
+│☹︎│▸ *BOT WORKTYPE* : ${mode}
+│☹︎│▸ *TOTAL PLUGINS* : ${cm.length} 
+│☹︎│▸ *AVAILABLE STORAGE* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+│☹︎│▸ *BOT RUNNING ON* : ${os.platform()}
+│☹︎│▸ *BOT THEME* : *BELTAH KE*
+┬╰──────────────✣
+╰─── ···▸💰LIKE A BOSS💰··──◆\n`;
+    
+let menuMsg = `
+╭─────────✣
+    *𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 V3.0.0*
+     *_Made with 💎_*
+╰─────────✣
+ *❄︎LIST PLUGINS❄︎*
+`;
+
+    for (const cat in coms) {
+        menuMsg += `╭𓄂𓄂𓄂𓄂𓄂𓄂𓄂☹︎✣ *${cat}* ✣☹︎`;
+        for (const cmd of coms[cat]) {
+            menuMsg += `
+│✇│ ${cmd} ✇`;
+        }
+        menuMsg += `
+╰────────────···▸▸ \n`
+    }
+
+    menuMsg += `
+◇            ◇
+*»»————————— ★ ——————————««*
+|⏣BELTAH-MD CREATED BY BELTAH TECH TEAM
+|⏣ *RELEASED ON : 02.06.2024*
+|⏣THANK YOU FOR CHOOSING *𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗*
+*»»—————————— ★ ——————————««*
+`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+});
